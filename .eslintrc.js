91814d5f2e369a2d85c161923f4f0f0dc099e7f6
lethekawa <lethekawa@gmail.com>
2021-06-14 21:00:23 +0900

    Add : eslint, prettier setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,35 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['plugin:react/recommended', 'google', 'prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'no-unused-vars': 1,
+    'object-curly-spacing': ['error', 'always'],
+    'require-jsdoc': [
+      'error',
+      {
+        require: {
+          FunctionDeclaration: false,
+          MethodDefinition: false,
+          ClassDeclaration: false,
+          ArrowFunctionExpression: false,
+          FunctionExpression: false,
+        },
+      },
+    ],
+  },
 };
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    printWidth: 200,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    arrowParens: 'always',
+    endOfLine: 'lf'
+};
